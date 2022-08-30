472293971083203c2c7fceb168001a795467c605
jungeun <kjjee99@naver.com>
2022-08-30 11:57:15 +0900

    Initial Commit

---
+++ src/test/java/com/travelmaker/travel_maker/TravelMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelmaker.travel_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
