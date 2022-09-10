a4e4ac11b81ff7b39ebecb3296d6e9636adbe49a
jungeun <kjjee99@naver.com>
2022-09-10 17:20:50 +0900

    [Feat] User Repository 클래스 생성

---
+++ src/main/java/com/travelmaker/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.travelmaker.repository;
+
+
+import com.travelmaker.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+}
