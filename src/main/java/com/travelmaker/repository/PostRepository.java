3db68bf4bbd83e510f683d26132bb7f9bca7c8a8
jungeun <kjjee99@naver.com>
2022-09-19 22:07:44 +0900

    [Feat] Annotation 추가

--- src/main/java/com/travelmaker/repository/PostRepository.java
+++ src/main/java/com/travelmaker/repository/PostRepository.java
@@ -2,6 +2,11 @@ package com.travelmaker.repository;
 
 import com.travelmaker.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface PostRepository extends JpaRepository<PostEntity, String> {
 }
