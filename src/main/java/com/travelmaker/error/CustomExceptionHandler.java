7c7adbf8b8754dce013b65eba6ffba735fec49d4
jungeun <kjjee99@naver.com>
2022-09-27 16:15:16 +0900

    [Feat] CustomExceptionHandler class 생성

---
+++ src/main/java/com/travelmaker/error/CustomExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.travelmaker.error;
+
+import com.travelmaker.entity.ErrorResponseEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e){
+        return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
+    }
+}
