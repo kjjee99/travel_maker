a56560a501864a367ae0ed1bfa8687b35dc426ad
jungeun <kjjee99@naver.com>
2022-10-17 17:05:51 +0900

    [Refactor] 필요없는 import문 제거

--- src/main/java/com/travelmaker/repository/PostRepository.java
+++ src/main/java/com/travelmaker/repository/PostRepository.java
@@ -1,6 +1,5 @@
 package com.travelmaker.repository;
 
-import com.travelmaker.dto.Post;
 import com.travelmaker.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
