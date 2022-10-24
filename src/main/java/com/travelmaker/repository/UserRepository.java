7829c926af30c23ac794ac6db4e2887187efb317
jungeun <kjjee99@naver.com>
2022-10-24 15:47:45 +0900

    [Comment] 주석 추가

--- src/main/java/com/travelmaker/repository/UserRepository.java
+++ src/main/java/com/travelmaker/repository/UserRepository.java
@@ -8,16 +8,19 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
 //@Transactional
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+    // 유저 검색
     @Query(value = "SELECT * from user u where u.user_id = :id", nativeQuery = true)
     Optional<UserEntity> findByUserId(@Param("id") String id);
 
+    // 회원정보 수정
+    @Transactional
     @Modifying
     @Query(value = "UPDATE user u SET u.user_id = :userId, " +
             "u.email = :email, u.password = :password, " +
@@ -31,6 +34,8 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
                           @Param("profileImg") String profileImg,
                           @Param("role") String role);
 
+    // 회원 탈
+    @Transactional
     @Modifying
     @Query(value = "UPDATE user u SET u.user_id=null, u.email=null, u.password=null," +
             "u.phone_number=null, u.profile_img=null, u.role=null " +
