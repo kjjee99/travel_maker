7be703b6de28704a5116ab1b8e468d2ea83d5ee2
jungeun <kjjee99@naver.com>
2022-11-08 17:56:21 +0900

    [Refactor] JSON 타입으로 변경

--- src/main/java/com/travelmaker/entity/PostEntity.java
+++ src/main/java/com/travelmaker/entity/PostEntity.java
@@ -1,17 +1,24 @@
 package com.travelmaker.entity;
 
+import com.travelmaker.dto.Figures;
+import com.travelmaker.dto.Roads;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @ToString
+@TypeDef(name = "json", typeClass = JsonType.class)
 @Table(name = "post")
 public class PostEntity {
 
@@ -33,15 +40,21 @@ public class PostEntity {
     private int like;
 
     @Column(name = "`figures`")
-    private String figures;
+    @Type(type="json")
+    private Figures figures;
+
     @Column(name = "post_img")
     private String post_img;
+
+    @Column(name = "roads")
+    @Type(type="json")
+    private List<Roads> roads;
 
     @Column(name = "created_at")
     private Date created_at;
 
     @Builder
-    public PostEntity(int id, String user_id, String title, String content, int like, String figures, String post_img, Date createdAt) {
+    public PostEntity(int id, String user_id, String title, String content, int like, Figures figures, String post_img, List<Roads> roads, Date createdAt) {
         this.id = id;
         this.user_id = user_id;
         this.title = title;
@@ -49,6 +62,7 @@ public class PostEntity {
         this.like = like;
         this.figures = figures;
         this.post_img = post_img;
+        this.roads = roads;
         this.created_at = createdAt;
     }
 }
