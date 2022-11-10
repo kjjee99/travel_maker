b722e4d157bfb6ae46fb408bbefa94741bfba9af
jungeun <kjjee99@naver.com>
2022-11-10 10:22:09 +0900

    [Feat] DB Table 생성

---
+++ src/main/resources/TravelMaker_table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(45) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `like` int DEFAULT '0',
+  `figures` json NOT NULL,
+  `post_img` varchar(255) NOT NULL,
+  `roads` text NOT NULL,
+  `created_at` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_user_id_idx` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `hashtags` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `post_hashtag` (
+  `post_id` int NOT NULL,
+  `tag_id` int NOT NULL,
+  PRIMARY KEY (`post_id`,`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+}
