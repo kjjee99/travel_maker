82233111e31053ec213578abfb604c388f2eb1f9
jungeun <kjjee99@naver.com>
2022-11-15 12:55:16 +0900

    [Refactor] Follow Service 구현

--- src/main/java/com/travelmaker/service/FollowService.java
+++ src/main/java/com/travelmaker/service/FollowService.java
@@ -1,4 +1,17 @@
 package com.travelmaker.service;
 
+import com.travelmaker.entity.UserEntity;
+
+import java.util.List;
+
 public interface FollowService {
+
+    /* 팔로우 */
+    boolean follow(String userId, String followId);
+
+    /* 팔로잉한 사람 목록 */
+    List<UserEntity> followingList(String userId);
+
+    /* 팔로우한 사람 목록 */
+    List<UserEntity> followerList(String userId);
 }
--- src/main/java/com/travelmaker/service/FollowServiceImpl.java
+++ src/main/java/com/travelmaker/service/FollowServiceImpl.java
@@ -1,7 +1,56 @@
 package com.travelmaker.service;
 
+import com.travelmaker.entity.FollowEntity;
+import com.travelmaker.entity.UserEntity;
+import com.travelmaker.error.CustomException;
+import com.travelmaker.error.ErrorCode;
+import com.travelmaker.repository.FollowRepository;
+import com.travelmaker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class FollowServiceImpl {
+public class FollowServiceImpl implements FollowService{
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    FollowRepository followRepository;
+
+    public boolean follow(String userId, String followId){
+        // 팔로우하는 유저
+        Optional<Integer> follower = userRepository.findIdByUserId(userId);
+        int followerId = follower.get();
+
+        // 팔로잉하는 유저
+        Optional<Integer> following = userRepository.findIdByUserId(followId);
+        int followingId = following.get();
+
+        // 저장하기
+        FollowEntity entity = FollowEntity.builder()
+                .userId(followerId)
+                .following(followingId)
+                .build();
+        FollowEntity result = followRepository.save(entity);
+        // ERROR
+        if(result.getUserId() == 0) new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+
+        return true;
+    }
+
+    /* 팔로잉한 사람 목록 */
+    public List<UserEntity> followingList(String userId){
+        List<UserEntity> followings = followRepository.followingList(userId);
+        return followings;
+    }
+
+    /* 팔로우한 사람 목록 */
+    public List<UserEntity> followerList(String userId){
+        List<UserEntity> followers = followRepository.followerList(userId);
+        return followers;
+    }
 }
