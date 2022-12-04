5c698c6b7fb6d1993e863d275a2bcedd70ce9840
jungeun <kjjee99@naver.com>
2022-12-05 00:21:52 +0900

    [Feat] AWS sdk 추가

--- src/main/java/com/travelmaker/TravelMakerApplication.java
+++ src/main/java/com/travelmaker/TravelMakerApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravelMakerApplication {
 
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEC2Metadata", "true");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TravelMakerApplication.class, args);
 	}
