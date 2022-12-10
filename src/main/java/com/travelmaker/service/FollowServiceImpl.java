923e183263a130b258bcd5b763b99393817df9f7
jungeun <kjjee99@naver.com>
2022-12-10 15:46:49 +0900

    [Refactor] 예외처리

--- src/main/java/com/travelmaker/service/FollowServiceImpl.java
+++ src/main/java/com/travelmaker/service/FollowServiceImpl.java
@@ -21,20 +21,23 @@ public class FollowServiceImpl implements FollowService{
     @Autowired
     FollowRepository followRepository;
 
-    // FIXME: throw Exception
+    /* 팔로우 */
+    @Override
     public boolean follow(String userId, String followId){
         // 팔로우하는 유저
-        Optional<Integer> follower = userRepository.findIdByUserId(userId);
-        int followerId = follower.get();
+        Optional<Integer> follower = Optional.ofNullable(userRepository.findIdByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND)));
+        int followerIdx = follower.get();
 
         // 팔로잉하는 유저
-        Optional<Integer> following = userRepository.findIdByUserId(followId);
-        int followingId = following.get();
+        Optional<Integer> following = Optional.ofNullable(userRepository.findIdByUserId(followId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND)));
+        int followingIdx = following.get();
 
         // 저장하기
         FollowEntity entity = FollowEntity.builder()
-                .userIdx(followerId)
-                .following(followingId)
+                .userIdx(followerIdx)
+                .following(followingIdx)
                 .build();
         FollowEntity result = followRepository.save(entity);
         // ERROR
@@ -44,20 +47,27 @@ public class FollowServiceImpl implements FollowService{
     }
 
     /* 팔로잉한 사람 목록 */
+    @Override
     public List<UserEntity> followingList(String userId){
-        Optional<Integer> id = userRepository.findIdByUserId(userId);
+        Optional<Integer> id = Optional.ofNullable(userRepository.findIdByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND)));
         int findId = id.get();
 
         List<UserEntity> followings = userRepository.followingList(findId);
+
+        if(followings.size() < 1)   throw new CustomException(ErrorCode.NULL_VALUE);
         return followings;
     }
 
     /* 팔로우한 사람 목록 */
+    @Override
     public List<UserEntity> followerList(String userId){
-        Optional<Integer> id = userRepository.findIdByUserId(userId);
+        Optional<Integer> id = Optional.ofNullable(userRepository.findIdByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND)));
         int findId = id.get();
 
         List<UserEntity> followers = userRepository.followerList(findId);
+        if(followers.size() < 1)   throw new CustomException(ErrorCode.NULL_VALUE);
         return followers;
     }
 }
