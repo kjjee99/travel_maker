627cfc30bd6ffaab559bd01f62b432d1e1f81474
jungeun <kjjee99@naver.com>
2022-12-12 17:22:52 +0900

    [Fix] SQL 오류 수정

--- src/main/java/com/travelmaker/repository/FollowRepository.java
+++ src/main/java/com/travelmaker/repository/FollowRepository.java
@@ -2,14 +2,18 @@ package com.travelmaker.repository;
 
 import com.travelmaker.entity.FollowEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 
 @Repository
 public interface FollowRepository extends JpaRepository<FollowEntity, String> {
+    @Transactional
+    @Modifying
     @Query(value = "delete from follow where useridx = :userIdx and following = :followingIdx", nativeQuery = true)
     Optional<Integer> unfollow(int userIdx, int followingIdx);
 }
