9be40c5564f5087d44f29a4295e4b7728b82f174
jungeun <kjjee99@naver.com>
2022-12-15 00:07:22 +0900

    [Docs] 주석 추가

--- src/main/java/com/travelmaker/repository/HashtagRepository.java
+++ src/main/java/com/travelmaker/repository/HashtagRepository.java
@@ -2,9 +2,11 @@ package com.travelmaker.repository;
 
 import com.travelmaker.entity.HashtagEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +19,7 @@ public interface HashtagRepository extends JpaRepository<HashtagEntity, String> 
             nativeQuery = true)
     Optional<HashtagEntity> findByName(@Param("name")String name);
 
+    // 해시태그로 게시글 검색
     @Query(value = "select tagname from hashtags h where idx in " +
             "(select tagid from post_hashtag where postid = :idx)", nativeQuery = true)
     String[] findTagsByPost(@Param("idx") int idx);
